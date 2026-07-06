a4abd69b8f365cddb53b9f833b6d114e26f5a26e
33788 <33788@LAPTOP-K63R976V>
2026-07-07 00:45:45 +0800

    添加项目文件。

---
+++ Snake/Config.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <SDL.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int MAX_FPS = 60;
+const int GRID_SIZE = 20;
+const SDL_Color TEXT_COLOR = { 0xff, 0xff, 0xff };
+const int SNAKE_START_LENGTH = 3;
